d2589dbeb2cbec4c9cc3368703327484369de286
pchivaru <127643419+pchivaru@users.noreply.github.com>
2023-11-18 18:30:53 -0500

    First deploy

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 #import importlib  
 #cv2 = importlib.import_module("opencv-python-headless")
 #import `opencv-python-headless' as cv2
 import cv2
 
-#from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 #from keras.utils import load_img, img_to_array
 from keras.models import Sequential, load_model, Model
 #from keras.layers import Conv2D, MaxPooling2D
@@ -14,6 +15,8 @@ from keras.models import Sequential, load_model, Model
 #from keras import backend as K
 #from keras.callbacks import ModelCheckpoint
 #from keras import regularizers
+
+from keras.applications.resnet import ResNet50, preprocess_input
 
 
 def change_to_bw(bytes_data, calibration_value):
@@ -29,6 +32,8 @@ st.title('Jetaire Dust Detection')
 
 modelpath='./model_current_best.h5'
 model = load_model(modelpath)
+images_dir = './imgs/'
+normed_dims = (500,500)
 
 
 
@@ -40,19 +45,45 @@ else:
     bytes_data = uploaded_file.read()
     CALIBRATION_VALUE= st.slider('Enter a calibration value', -70, 0, -35)
 
-    col1, col2 = st.columns(2)
+    col1, col2= st.columns(2)
     with col1:
-        st.header("Original")
+        st.subheader("Original")
         st.image(bytes_data)
 
     with col2:
-        st.header("Processed")
+        st.subheader("Processed")
         (image_bw,thres)=change_to_bw(bytes_data, CALIBRATION_VALUE)
+        cv2.imwrite('./imgs/0/curr_image.jpg', image_bw)
         st.image(image_bw)
 
+    test_datagen = ImageDataGenerator(dtype='float32',
+                                preprocessing_function = preprocess_input)
 
-if st.button('Analyze'):
-    st.write('Analizing...')
+    test_generator = test_datagen.flow_from_directory(
+        images_dir,
+        target_size=normed_dims,
+        batch_size=1,
+        shuffle=False,
+        #class_mode='binary'
+        class_mode="categorical"
+        )
+    
+    test_generator.reset()
+    X_te, y_te = test_generator.next()
+
+    res = model.predict(np.expand_dims(X_te[0], axis = 0))
+
+    cat_percentages = np.array([0, 25, 50, 100])
+    y_percentage = res.dot(cat_percentages)
+
+    color = 'red'
+    if y_percentage[0] < 25:
+        color = 'green'
+
+    st.header(f'Dust percentage: :{color}[{round(y_percentage[0],2)}%]')
+
+
+
 
 
 
