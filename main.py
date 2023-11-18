31ed9f15fd349b17322b757420f47a16493c38aa
pchivaru <127643419+pchivaru@users.noreply.github.com>
2023-11-18 15:36:17 -0500

    Filter

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import cv2
 
-st.title('Uber pickups in NYC')
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import load_img, img_to_array
+from keras.models import Sequential, load_model, Model
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+from keras import regularizers
+from glob import glob
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+import matplotlib.pyplot as plt
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+from IPython.display import SVG, display, clear_output
+from keras.utils.vis_utils import model_to_dot
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+def change_to_bw(bytes_data, calibration_value):
+  im_gray = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_GRAYSCALE) 
+  (thresh, im_bw) = cv2.threshold(im_gray, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+  thresh += calibration_value
+  im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+  #im_bw = np.array(im_bw)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+  return(im_bw, thresh)
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+st.title('Jetaire Dust Detection')
+
+modelpath='./model_current_best.h5'
+model = load_model(modelpath)
+
+
+
+uploaded_file = st.file_uploader("Upload an image", type=['png','jpg'])
+
+if uploaded_file is None:
+    CALIBRATION_VALUE = -35
+else:
+    bytes_data = uploaded_file.read()
+    CALIBRATION_VALUE= st.slider('Enter a calibration value', -70, 0, -35)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("Original")
+        st.image(bytes_data)
+
+    with col2:
+        st.header("Processed")
+        (image_bw,thres)=change_to_bw(bytes_data, CALIBRATION_VALUE)
+        st.image(image_bw)
+
+
+if st.button('Analyze'):
+    st.write('Analizing...')
+
+
+
